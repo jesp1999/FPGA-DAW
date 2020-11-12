e01599d90a7c192f607aeb1849d6a99f8c29f5de
Julian Espada <jesp1999@mit.edu>
2020-11-12 01:05:18 -0500

    Added debounce module

---
+++ FPGA DAW/FPGA DAW.srcs/sources_1/new/debounce.sv
@@ -0,0 +1,21 @@
+`timescale 1ns / 1ps
+
+module debounce (input logic clk_in,
+                 input logic rst_in,
+                 input logic noisy_in,
+                 output logic clean_out);
+
+   logic [19:0] count;
+   logic new_input;
+
+   always_ff @(posedge clk_in)
+     if (rst_in) begin 
+        new_input <= noisy_in; 
+        clean_out <= noisy_in; 
+        count <= 0; end
+     else if (noisy_in != new_input) begin new_input<=noisy_in; count <= 0; end
+     else if (count == 1000000) clean_out <= new_input;
+     else count <= count+1;
+
+
+endmodule

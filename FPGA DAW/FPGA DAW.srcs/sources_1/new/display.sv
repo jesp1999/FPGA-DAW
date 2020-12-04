191641f4801aff3d4894dbd5d6f98c79754f76c6
Julian Espada <jesp1999@mit.edu>
2020-12-04 16:05:59 -0500

    Showing 4 keyboards

--- FPGA DAW/FPGA DAW.srcs/sources_1/new/display.sv
+++ FPGA DAW/FPGA DAW.srcs/sources_1/new/display.sv
@@ -5,7 +5,7 @@ module display(
                input logic rst_in,
                input logic [10:0] hcount_in,
                input logic [9:0] vcount_in,
-               input logic [12:0] keys,
+               input logic [12:0] keys [3:0],
                input logic waveform,
                output logic [11:0] pixel_out
                );
@@ -15,32 +15,99 @@ module display(
         logic [11:0] pixel_reg2;
         logic [11:0] pixel_reg3;
         
-        parameter WHITE_KEY_WIDTH = 30;
-        parameter WHITE_KEY_HEIGHT = 96;
-        parameter BLACK_KEY_WIDTH = 14;
-        parameter BLACK_KEY_HEIGHT = 64;
-       
-        parameter KEY_SPACING = 2;
-        parameter BLACK_KEY_OFFSET = 3 * (WHITE_KEY_WIDTH + KEY_SPACING) / 4;
-        
-        parameter KEYBOARD_ORIGIN_X = 32;
-        parameter KEYBOARD_ORIGIN_Y = 600;
-        
-        parameter WHITE_KEY_COLOR = 12'hFFF;
-        parameter BLACK_KEY_COLOR = 12'h000;
-        parameter WHITE_SELECTED_COLOR = 12'hF00;
-        parameter BLACK_SELECTED_COLOR = 12'h00F;
         parameter BACKGROUND_COLOR = 12'hDE3;
         
         ///////////////////////////
         //KEYBOARD//DRAWING//CODE//
         ///////////////////////////
         
+        logic [11:0] keyboard_track0_pixel, keyboard_track1_pixel, keyboard_track2_pixel, keyboard_track3_pixel;
+        keyboard_blob #(.KEYBOARD_ORIGIN_X(4), .KEYBOARD_ORIGIN_Y(600))
+        blob_keyboard_track0 (.keys(keys[0]), .hcount_in(hcount_in), .vcount_in(vcount_in), .pixel_in(BACKGROUND_COLOR), .pixel_out(keyboard_track0_pixel));
+        
+        keyboard_blob #(.KEYBOARD_ORIGIN_X(4 + 238 + 18), .KEYBOARD_ORIGIN_Y(600))
+        blob_keyboard_track1 (.keys(keys[1]), .hcount_in(hcount_in), .vcount_in(vcount_in), .pixel_in(BACKGROUND_COLOR), .pixel_out(keyboard_track1_pixel));
+        
+        keyboard_blob #(.KEYBOARD_ORIGIN_X(4 + 476 + 36), .KEYBOARD_ORIGIN_Y(600))
+        blob_keyboard_track2 (.keys(keys[2]), .hcount_in(hcount_in), .vcount_in(vcount_in), .pixel_in(BACKGROUND_COLOR), .pixel_out(keyboard_track2_pixel));
+        
+        keyboard_blob #(.KEYBOARD_ORIGIN_X(4 + 714 + 54), .KEYBOARD_ORIGIN_Y(600))
+        blob_keyboard_track3 (.keys(keys[3]), .hcount_in(hcount_in), .vcount_in(vcount_in), .pixel_in(BACKGROUND_COLOR), .pixel_out(keyboard_track3_pixel));
+       
+       
+        //////////////////////////////////////
+        //WAVEFORM//SELECTION//DRAWING//CODE//
+        //////////////////////////////////////
+       
+//        parameter WAVE_ICON_SELECTION_WIDTH = 48;
+//        parameter WAVE_ICON_SELECTION_HEIGHT = 48;
+       
+//        parameter WAVE_ICON_SELECTION_OFFSET = 16;
+       
+//        parameter SINE_WAVE_ICON_ORIGIN_X = 32;
+//        parameter SINE_WAVE_ICON_ORIGIN_Y = 32;
+//        parameter TRNG_WAVE_ICON_ORIGIN_X = 32;
+//        parameter TRNG_WAVE_ICON_ORIGIN_Y = 96;
+       
+//        parameter WAVE_ICON_SELECTED_COLOR = 12'h00F;
+       
+       
+//        selectable_blob  #(.WIDTH(WAVE_ICON_SELECTION_WIDTH), .HEIGHT(WAVE_ICON_SELECTION_HEIGHT), .COLOR(BACKGROUND_COLOR), .SELECTED_COLOR(WAVE_ICON_SELECTED_COLOR))
+//        blob_sinewave_selection (.x_in(SINE_WAVE_ICON_ORIGIN_X - WAVE_ICON_SELECTION_OFFSET), .y_in(SINE_WAVE_ICON_ORIGIN_Y - WAVE_ICON_SELECTION_OFFSET), .hcount_in(hcount_in), .vcount_in(vcount_in), .selection(~waveform), .pixel_in(sine_icon_pixel), .pixel_out(sine_img_pixel));
+      
+//        sinewave_blob blob_sineWaveform (.pixel_clk_in(clk_in), .hcount_in(hcount_in), .vcount_in(vcount_in), .x_in(SINE_WAVE_ICON_ORIGIN_X), .y_in(SINE_WAVE_ICON_ORIGIN_Y), .pixel_in(sine_img_pixel), .pixel_out(trng_icon_pixel));
+
+//        selectable_blob  #(.WIDTH(WAVE_ICON_SELECTION_WIDTH), .HEIGHT(WAVE_ICON_SELECTION_HEIGHT), .COLOR(BACKGROUND_COLOR), .SELECTED_COLOR(WAVE_ICON_SELECTED_COLOR))
+//        blob_trngwave_selection (.x_in(TRNG_WAVE_ICON_ORIGIN_X - WAVE_ICON_SELECTION_OFFSET), .y_in(TRNG_WAVE_ICON_ORIGIN_Y - WAVE_ICON_SELECTION_OFFSET), .hcount_in(hcount_in), .vcount_in(vcount_in), .selection(waveform), .pixel_in(trng_icon_pixel), .pixel_out(trng_img_pixel));
+       
+//        trngwave_blob blob_trngWaveform (.pixel_clk_in(clk_in), .hcount_in(hcount_in), .vcount_in(vcount_in), .x_in(TRNG_WAVE_ICON_ORIGIN_X), .y_in(TRNG_WAVE_ICON_ORIGIN_Y), .pixel_in(trng_img_pixel), .pixel_out(pixel_out));
+       
+        
+        always_ff @(posedge clk_in) begin
+            if (keyboard_track0_pixel != BACKGROUND_COLOR) begin
+                pixel_out <= keyboard_track0_pixel;
+            end else if (keyboard_track1_pixel != BACKGROUND_COLOR) begin
+                pixel_out <= keyboard_track1_pixel;
+            end else if (keyboard_track2_pixel != BACKGROUND_COLOR) begin
+                pixel_out <= keyboard_track2_pixel;
+            end else if (keyboard_track3_pixel != BACKGROUND_COLOR) begin
+                pixel_out <= keyboard_track3_pixel;
+            end else begin
+                pixel_out <= BACKGROUND_COLOR;
+            end
+//            sine_img_pixel <= pixel_reg1;
+//            pixel_reg3 <= pixel_reg2;
+//            pixel_reg2 <= pixel_reg1;
+        end
+       
+endmodule
+
+module keyboard_blob
+   #(parameter KEYBOARD_ORIGIN_X = 0,
+               KEYBOARD_ORIGIN_Y = 0,
+               WHITE_KEY_WIDTH = 28,
+               WHITE_KEY_HEIGHT = 96,
+               BLACK_KEY_WIDTH = 14,
+               BLACK_KEY_HEIGHT = 64,
+               KEY_SPACING = 2,
+               WHITE_KEY_COLOR = 12'hFFF,
+               BLACK_KEY_COLOR = 12'h000,
+               WHITE_SELECTED_COLOR = 12'hF00,
+               BLACK_SELECTED_COLOR = 12'h00F)
+   (input logic [12:0] keys,
+    input logic [10:0] hcount_in,
+    input logic [9:0] vcount_in,
+    input logic [11:0] pixel_in,
+    output logic [11:0] pixel_out);
+   
+    localparam BLACK_KEY_OFFSET = 3 * (WHITE_KEY_WIDTH + KEY_SPACING) / 4;
+    
+    
         logic [11:0] CPix, CSharpPix, DPix, DSharpPix, EPix, FPix, FSharpPix, GPix, GSharpPix, APix, ASharpPix, BPix;
         logic [11:0] sine_img_pixel, sine_icon_pixel, trng_img_pixel, trng_icon_pixel;
          
         selectable_blob  #(.WIDTH(WHITE_KEY_WIDTH), .HEIGHT(WHITE_KEY_HEIGHT), .COLOR(WHITE_KEY_COLOR), .SELECTED_COLOR(WHITE_SELECTED_COLOR))
-        blob_C (.x_in(KEYBOARD_ORIGIN_X), .y_in(KEYBOARD_ORIGIN_Y), .hcount_in(hcount_in), .vcount_in(vcount_in), .selection(keys[12]), .pixel_in(BACKGROUND_COLOR), .pixel_out(CPix));
+        blob_C (.x_in(KEYBOARD_ORIGIN_X), .y_in(KEYBOARD_ORIGIN_Y), .hcount_in(hcount_in), .vcount_in(vcount_in), .selection(keys[12]), .pixel_in(pixel_in), .pixel_out(CPix));
         
         selectable_blob  #(.WIDTH(WHITE_KEY_WIDTH), .HEIGHT(WHITE_KEY_HEIGHT), .COLOR(WHITE_KEY_COLOR), .SELECTED_COLOR(WHITE_SELECTED_COLOR))
         blob_D (.x_in(KEYBOARD_ORIGIN_X + (WHITE_KEY_WIDTH + KEY_SPACING)), .y_in(KEYBOARD_ORIGIN_Y), .hcount_in(hcount_in), .vcount_in(vcount_in), .selection(keys[10]), .pixel_in(CPix), .pixel_out(DPix));
@@ -76,45 +143,11 @@ module display(
         blob_ASharp (.x_in(KEYBOARD_ORIGIN_X + 5*(WHITE_KEY_WIDTH + KEY_SPACING) + BLACK_KEY_OFFSET), .y_in(KEYBOARD_ORIGIN_Y), .hcount_in(hcount_in), .vcount_in(vcount_in), .selection(keys[2]), .pixel_in(BPix), .pixel_out(ASharpPix));
        
         selectable_blob  #(.WIDTH(WHITE_KEY_WIDTH), .HEIGHT(WHITE_KEY_HEIGHT), .COLOR(WHITE_KEY_COLOR), .SELECTED_COLOR(WHITE_SELECTED_COLOR))
-        blob_C2 (.x_in(KEYBOARD_ORIGIN_X + 7*(WHITE_KEY_WIDTH + KEY_SPACING)), .y_in(KEYBOARD_ORIGIN_Y), .hcount_in(hcount_in), .vcount_in(vcount_in), .selection(keys[0]), .pixel_in(ASharpPix), .pixel_out(sine_icon_pixel));
-       
-       
-        //////////////////////////////////////
-        //WAVEFORM//SELECTION//DRAWING//CODE//
-        //////////////////////////////////////
-       
-        parameter WAVE_ICON_SELECTION_WIDTH = 48;
-        parameter WAVE_ICON_SELECTION_HEIGHT = 48;
-       
-        parameter WAVE_ICON_SELECTION_OFFSET = 16;
-       
-        parameter SINE_WAVE_ICON_ORIGIN_X = 32;
-        parameter SINE_WAVE_ICON_ORIGIN_Y = 32;
-        parameter TRNG_WAVE_ICON_ORIGIN_X = 32;
-        parameter TRNG_WAVE_ICON_ORIGIN_Y = 96;
-       
-        parameter WAVE_ICON_SELECTED_COLOR = 12'h00F;
-       
-       
-        selectable_blob  #(.WIDTH(WAVE_ICON_SELECTION_WIDTH), .HEIGHT(WAVE_ICON_SELECTION_HEIGHT), .COLOR(BACKGROUND_COLOR), .SELECTED_COLOR(WAVE_ICON_SELECTED_COLOR))
-        blob_sinewave_selection (.x_in(SINE_WAVE_ICON_ORIGIN_X - WAVE_ICON_SELECTION_OFFSET), .y_in(SINE_WAVE_ICON_ORIGIN_Y - WAVE_ICON_SELECTION_OFFSET), .hcount_in(hcount_in), .vcount_in(vcount_in), .selection(~waveform), .pixel_in(sine_icon_pixel), .pixel_out(sine_img_pixel));
-      
-        sinewave_blob blob_sineWaveform (.pixel_clk_in(clk_in), .hcount_in(hcount_in), .vcount_in(vcount_in), .x_in(SINE_WAVE_ICON_ORIGIN_X), .y_in(SINE_WAVE_ICON_ORIGIN_Y), .pixel_in(sine_img_pixel), .pixel_out(trng_icon_pixel));
-
-        selectable_blob  #(.WIDTH(WAVE_ICON_SELECTION_WIDTH), .HEIGHT(WAVE_ICON_SELECTION_HEIGHT), .COLOR(BACKGROUND_COLOR), .SELECTED_COLOR(WAVE_ICON_SELECTED_COLOR))
-        blob_trngwave_selection (.x_in(TRNG_WAVE_ICON_ORIGIN_X - WAVE_ICON_SELECTION_OFFSET), .y_in(TRNG_WAVE_ICON_ORIGIN_Y - WAVE_ICON_SELECTION_OFFSET), .hcount_in(hcount_in), .vcount_in(vcount_in), .selection(waveform), .pixel_in(trng_icon_pixel), .pixel_out(trng_img_pixel));
-       
-        trngwave_blob blob_trngWaveform (.pixel_clk_in(clk_in), .hcount_in(hcount_in), .vcount_in(vcount_in), .x_in(TRNG_WAVE_ICON_ORIGIN_X), .y_in(TRNG_WAVE_ICON_ORIGIN_Y), .pixel_in(trng_img_pixel), .pixel_out(pixel_out));
-       
+        blob_C2 (.x_in(KEYBOARD_ORIGIN_X + 7*(WHITE_KEY_WIDTH + KEY_SPACING)), .y_in(KEYBOARD_ORIGIN_Y), .hcount_in(hcount_in), .vcount_in(vcount_in), .selection(keys[0]), .pixel_in(ASharpPix), .pixel_out(pixel_out));
         
-//        always_ff @(posedge clk_in) begin
-//            sine_img_pixel <= pixel_reg1;
-//            pixel_reg3 <= pixel_reg2;
-//            pixel_reg2 <= pixel_reg1;
-//        end
-       
+        
 endmodule
-
+   
 
 module selectable_blob
    #(parameter WIDTH = 64,               // default width: 64 pixels

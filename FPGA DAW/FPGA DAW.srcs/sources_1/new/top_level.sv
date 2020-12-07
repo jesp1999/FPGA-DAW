0726c628e51b3d40f04c8a10262d58bc870ce71a
Julian Espada <jesp1999@mit.edu>
2020-12-07 16:25:31 -0500

    Prettied up top_level and removed ILA

--- FPGA DAW/FPGA DAW.srcs/sources_1/new/top_level.sv
+++ FPGA DAW/FPGA DAW.srcs/sources_1/new/top_level.sv
@@ -28,25 +28,19 @@ module top_level(
                     output logic aud_pwm,
                     output logic aud_sd);
     
-    parameter SAMPLE_COUNT = 1354;//gets approximately (will generate audio at approx 48 kHz sample rate.
- 
-    logic clean_btnu, clean_btnd, clean_btnl, clean_btnr; 
- 
-    logic [15:0] sample_counter;
-    logic sample_trigger;
-    logic enable;
-    logic [7:0] recorder_data;             
-    logic [7:0] vol_out;
-    logic pwm_val; //pwm signal (HI/LO)
- 
-    logic clean_record_btn;
     
+    // Modulate clock signal to 65MHz to support 1024x720 VGA @ 60Hz and allow more slack for computations
     logic clk_65mhz;
     logic clk_65mhz_locked;
     clk_wiz_65mhz clk_65mhz_mod(.clk_in1(clk_100mhz), .reset(0), .clk_65mhz(clk_65mhz), .locked(clk_65mhz_locked));
+    
+    
+    // Sampling trigger for (approximately) 48kHz sample rate
+    parameter SAMPLE_COUNT = 1354;
  
-    assign aud_sd = 1;
-    //assign led = sw; //just to look pretty 
+    logic [15:0] sample_counter;
+    logic sample_trigger;
+    
     assign sample_trigger = (sample_counter == SAMPLE_COUNT);
  
     always_ff @(posedge clk_65mhz)begin
@@ -56,7 +50,18 @@ module top_level(
             sample_counter <= sample_counter + 16'b1;
         end
     end
- 
+    
+    
+    // Debouncer for on-board non-reset buttons (btnc is not debounced as it is a reset, it shouldn't be triggered frequently and debouncing isn't critical
+    logic clean_btnu, clean_btnd, clean_btnl, clean_btnr;
+    
+    debounce db_btnu (.clk_in(clk_65mhz), .rst_in(btnc), .noisy_in(btnu), .clean_out(clean_btnu));
+    debounce db_btnd (.clk_in(clk_65mhz), .rst_in(btnc), .noisy_in(btnd), .clean_out(clean_btnd));
+    debounce db_btnl (.clk_in(clk_65mhz), .rst_in(btnc), .noisy_in(btnl), .clean_out(clean_btnl));
+    debounce db_btnr (.clk_in(clk_65mhz), .rst_in(btnc), .noisy_in(btnr), .clean_out(clean_btnr));
+    
+    
+    // Recording, Metronome, and Track modules
     logic [1:0] track;
     logic [7:0] raw_audio_out [3:0];
     logic [12:0] notes;
@@ -70,16 +75,20 @@ module top_level(
     logic [7:0] latest_valid_beat [3:0];
     logic metronome_main, metronome_secondary;
     logic [7:0] metronome_raw_audio;
+    logic recording;
+    
     assign volume_to_play[0] = sw[15:13];
     assign volume_to_play[1] = sw[12:10];
     assign volume_to_play[2] = sw[9:7];
     assign volume_to_play[3] = sw[6:4];
+    
+    assign recording = sw[0];
  
     recorder my_rec (.clk_in(clk_65mhz), .rst_in(btnc), .rst_beat_count(clean_btnl), .rst_track(clean_btnr),
         .notes_in(notes),
         .octave_in(octave),
         .instrument_in(instrument),
-        .record(sw[0]), // CHANGE LATER
+        .record(recording),
         .track(track),
         .beat_count(beat_count),
         .notes_out(notes_to_play),
@@ -123,21 +132,24 @@ module top_level(
         .signal_secondary(metronome_secondary),
         .amp_out(metronome_raw_audio));
         
-//    volume_control vc (.vol_in(sw[15:13]),
-//                       .signal_in(raw_audio_out[0]), .signal_out(vol_out));
+        
+    // PWM controller for audio jack output
+    logic [7:0] vol_out;
+    logic pwm_val; //pwm signal (HI/LO)
+ 
+    assign aud_sd = 1;
+    
     pwm (.clk_in(clk_65mhz), .rst_in(btnc), .level_in({~vol_out[7],vol_out[6:0]}), .pwm_out(pwm_val));
     assign aud_pwm = pwm_val?1'bZ:1'b0; 
     
-//    logic [87:0] notes;
+    
+    // Controller for USB keyboard input
     logic [31:0] raw_keyboard;
-
-    debounce db_btnu (.clk_in(clk_65mhz), .rst_in(btnc), .noisy_in(btnu), .clean_out(clean_btnu));
-    debounce db_btnd (.clk_in(clk_65mhz), .rst_in(btnc), .noisy_in(btnd), .clean_out(clean_btnd));
-    debounce db_btnl (.clk_in(clk_65mhz), .rst_in(btnc), .noisy_in(btnl), .clean_out(clean_btnl));
-    debounce db_btnr (.clk_in(clk_65mhz), .rst_in(btnc), .noisy_in(btnr), .clean_out(clean_btnr));
 
     input_handler input_handler_mod (.clk_in(clk_65mhz), .rst_in(btnc), .data_clk_in(ps2_clk), .data_in(ps2_data), .notes_out(notes), .octave(octave), .raw_out(raw_keyboard));
     
+    
+    // Waveform and Track selection modules
     logic waveform_select_signal;
     logic track_select_signal;
     assign waveform_select_signal = clean_btnd;
@@ -147,6 +159,8 @@ module top_level(
     
     track_select track_sel_mod (.clk_in(clk_65mhz), .rst_in(btnc), .signal(track_select_signal), .track(track));
     
+    
+    // Audio mixer for combining audio and metronome tracks
     mixer mixer_mod (.clk_in(clk_65mhz), .rst_in(btnc), .volume(volume_to_play), .met_volume(sw[3:1]),
                      .audio0_in(raw_audio_out[0]), 
                      .audio1_in(raw_audio_out[1]), 
@@ -154,17 +168,20 @@ module top_level(
                      .audio3_in(raw_audio_out[3]), 
                      .metronome_in(metronome_raw_audio), .audio_out(vol_out));
     
-    logic ch;
-    seven_seg_controller seven_seg_mod (.clk_in(clk_65mhz), .rst_in(btnc), .val_in({latest_valid_beat[0], metronome_raw_audio, 7'b0, metronome_main, beat_count}), .cat_out({ch,cg,cf,ce,cd,cc,cb,ca}), .an_out(an));
-//    effects effects_mod ();
     
+    // 7-segment and LED debuggers
+    logic ch; //alphabetically-pleasing unused cat_out logic
+    seven_seg_controller seven_seg_mod (.clk_in(clk_65mhz), .rst_in(btnc), .val_in({latest_valid_beat[0], metronome_raw_audio, 7'b0, metronome_main, beat_count}),
+    .cat_out({ch,cg,cf,ce,cd,cc,cb,ca}), .an_out(an));
     
     assign led[2:0] = {metronome_main,track};
     
-    // hcount represents hcount 2 ticks in the future, hcount_reg2 is the current hcount (actually displayed)
-    // this allows to predict what the image should be and initiate a rom read early
-    logic [10:0] hcount_t_plus_2, hcount_t_plus_1, hcount_t;    // pixel on current line
-    logic [9:0] vcount_t_plus_2, vcount_t_plus_1, vcount_t;     // line number
+    
+    // XVGA and Display controller modules
+    // hcount_t_plus_2 represents hcount 2 ticks in the future, hcount_t is the current hcount (actually displayed)
+    // This allows to predict what the image should be and initiate a rom read early
+    logic [10:0] hcount_t_plus_2, hcount_t_plus_1, hcount_t;
+    logic [9:0] vcount_t_plus_2, vcount_t_plus_1, vcount_t;
     logic hsync, vsync;
     logic [11:0] pixel;
     logic [11:0] rgb;
@@ -173,9 +190,6 @@ module top_level(
     display display_mod (.clk_in(clk_65mhz), .rst_in(btnc), .keys(notes_to_play), .track(track), .beat(beat_count), .latest_valid_beat(latest_valid_beat), .octave(octave_to_play), .volume(volume_to_play), .instrument(instrument_to_play),
                          .vcount_curr_in(vcount_t), .hcount_curr_in(hcount_t), .vcount_next_in(vcount_t_plus_2), .hcount_next_in(hcount_t_plus_2), .pixel_out(pixel));
     
-    logic border = (hcount_t==0 | hcount_t==1023 | vcount_t==0 | vcount_t==767 |
-                   hcount_t == 512 | vcount_t == 384);
-
     logic b,hs,vs;
     always_ff @(posedge clk_65mhz) begin
         hcount_t <= hcount_t_plus_1;
@@ -195,8 +209,4 @@ module top_level(
 
     assign vga_hs = ~hs;
     assign vga_vs = ~vs;
-    
-    
-    //ila_0  ila_mod (.clk(clk_65mhz), .probe0(ps2_clk), .probe1(raw_keyboard));
-    
 endmodule
